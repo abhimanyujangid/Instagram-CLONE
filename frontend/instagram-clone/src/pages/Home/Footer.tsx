c5badba7e9591b3984a76f3c483d928f0a18f0d8
abhimanyujangid <abhimanyujangid79@gmail.com>
2025-01-14 01:31:51 +0530

    Update Footer component: remove unnecessary transition effect on user avatar and add cursor pointer to logout icon

--- frontend/instagram-clone/src/pages/Home/Footer.tsx
+++ frontend/instagram-clone/src/pages/Home/Footer.tsx
@@ -12,13 +12,13 @@ const Footer = () => {
   return (
         <div className="p-4  border-neutral-200/20 border-t flex items-center">
             <div className="flex items-center cursor-pointer" onClick={()=>{navigate('/profile')}}>
-              <img src="https://avatar.iran.liara.run/public" alt="User" className="w-10 h-10 rounded-full transition-opacity duration-300 opacity-100" loading="lazy" />
+              <img src="https://avatar.iran.liara.run/public" alt="User" className="w-10 h-10 rounded-full " loading="lazy" />
               <div className="ml-3">
                 <p className="text-sm font-medium text-neutral-700">Username</p>
                 <p className="text-xs text-neutral-500">john@example.com</p>
               </div>
             </div>
-            <LogInIcon className="w-5 h-5 ml-auto text-neutral-500" onClick={handelLogout} />
+            <LogInIcon className="w-5 h-5 ml-auto text-neutral-500 cursor-pointer" onClick={handelLogout} />
           </div>
   )
 }
